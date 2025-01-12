51a88e4d4d8276255667dac30856c18074514ca7
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2025-01-13 01:36:57 +0530

    Adding image assets and label to storyboard,

---
+++ IamRich/IamRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Abdul Saleem Mohamed Faheem on 2025-01-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
