b683e47048158d1b831a9cde334bcbda4cf8617b
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2025-01-20 00:57:52 +0530

    Randomly showing dices,

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -12,19 +12,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")]
+    
     // Executed when view is loaded, similar to onCreate in android I guess
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        // diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
         // diceImageView1.alpha = 0.5
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        // diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
 
     @IBAction func onRollDice(_ sender: UIButton) {
         NSLog("Roll clicked!")
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
+        // diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView1.image = diceArray.randomElement()
+        // diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray.randomElement()
     }
     
 }
